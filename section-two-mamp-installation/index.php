e6f0e9a095e8ce73252975405dbc702c6758609c
daywison <daywisonsilvasou@gmail.com>
2024-04-29 01:32:20 -0300

    init project

---
+++ section-two-mamp-installation/index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>1. Básico</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php">
+                                Exercício A
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício B
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício C
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>2. Tipos</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php">
+                                Exercício A
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício B
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício C
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo azul">
+                    <h3>3. Variáveis</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php">
+                                Exercício A
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício B
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício C
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo roxo">
+                    <h3>4. Controle</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php">
+                                Exercício A
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício B
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício C
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo laranja">
+                    <h3>5. Array</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php">
+                                Exercício A
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício B
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício C
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo verde-escuro">
+                    <h3>6. Repetições</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php">
+                                Exercício A
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício B
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício C
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho-escuro">
+                    <h3>7. Funções</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php">
+                                Exercício A
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício B
+                            </a>
+                            <a href="exercicio.php">
+                                Exercício C
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y'); ?>
+    </footer>
+</body>
+
+</html>
